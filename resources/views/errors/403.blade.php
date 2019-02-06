d0b30283cdbfddc2aeb1dd79273566b0ff851bda
anuryadi <nuryadiari8@gmail.com>
2019-02-06 13:41:03 +0700

    Gate Error

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -3,7 +3,7 @@
     <div class="d-flex flex-row justify-content-center">
         <div class="col-md-6 text-center">
             <div class="alert alert-danger">
-                <h1>401</h1>
+                <h1>403</h1>
                 <h4>{{$exception->getMessage()}}</h4>
             </div>
         </div>
